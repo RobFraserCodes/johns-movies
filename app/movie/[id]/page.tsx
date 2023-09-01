174b8277e21d7178821c631b63d0422751dd8b9c
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-09-01 13:05:04 +0100

    movie details api working :)

---
+++ app/movie/[id]/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { movie } from '@/type';
+
+export default function MovieDetailPage({ params }: { params: { id: string } }) {
+  const [movieDetails, setMovieDetails] = useState<movie>(null);
+  const url = `https://api.themoviedb.org/3/movie/${params.id}?language=en-US`;
+  const token = process.env.NEXT_PUBLIC_TMDB_TOKEN;
+  const auth = `Bearer ${token}`;
+    
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: auth
+    }
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+          setMovieDetails(json);
+        })
+        .catch(err => console.error('error:' + err));
+    }
+  }, [params.id]);
+
+  if (!movieDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className='container flex-col py-16'>
+      <Image 
+        src={movieDetails.poster_path ? `https://image.tmdb.org/t/p/w300${movieDetails.poster_path}` : 'path_to_default_image.jpg'} alt={movieDetails.title} 
+        className='rounded-md'
+        alt={movieDetails.title}
+        width={300}
+        height={450}
+      /> 
+      <h1>{movieDetails.title}</h1>
+      <p>{movieDetails.overview}</p>
+      {/* Add other movie details as desired */}
+    </section>
+  );
+}
