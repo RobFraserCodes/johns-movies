f16d2f0c5b50098d976b5f4c70c0015ab286eb38
Code Inverness <99823868+RobFraserCodes@users.noreply.github.com>
2023-09-04 08:49:22 +0100

    Update page.tsx
    got the styling and elements postitioned the way i want them

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -2,6 +2,9 @@
 
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { ArrowUturnLeftIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { Button, buttonVariants } from '@/components/ui/button';
 
 interface movie {
   title: string;
@@ -43,22 +46,35 @@ export default function MovieDetailPage({ params }: { params: { id: string } }) 
   const backdropUrl = movieDetails.backdrop_path ? `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}` : 'path_to_default_image.jpg';
 
   return (
-    <section className="container py-16 h-screen" style={{ backgroundImage: `url(${backdropUrl})`, backgroundSize: 'cover' }}>
-      <div className='flex flex-col lg:flex-row text-white justify-center'>
-        <div className="m-8 w-1/2">
+    <section 
+      className="relative flex justify-center items-center h-screen py-16" 
+      style={{ 
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${backdropUrl})`,
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center' 
+      }}>
+        <div className="absolute inset-0 ">
+          <Link href="/">
+            <ArrowUturnLeftIcon className='w-8 h-8 text-white m-8 lg:ml-40' />    
+          </Link>
+        </div>
+
+      <div className='flex flex-col lg:flex-row text-white justify-center items-center w-full max-w-screen-xl'>
+        <div className="m-4 md:m-8 w-1/2">
           <h1>{movieDetails.title}</h1>
           <p>{movieDetails.overview}</p>
         </div>
-        <div className='w-1/2'>
+        <div className='flex justify-center items-center m-4 lg:m-8 w-1/2 p-8'>
           <Image 
-            src={movieDetails.poster_path ? `https://image.tmdb.org/t/p/w300${movieDetails.poster_path}` : 'path_to_default_image.jpg'} 
+            src={movieDetails.poster_path ? `https://image.tmdb.org/t/p/w780${movieDetails.poster_path}` : 'path_to_default_image.jpg'} 
             alt={movieDetails.title} 
-            className='rounded-md m-8'
+            className='rounded-md'
             width={300}
             height={450}
           /> 
         </div>
       </div>
     </section>
-  );
+  );  
+  
 }
