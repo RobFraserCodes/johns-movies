e401cc426f8613f3bf7b7a129b04ce99b9a858bf
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-09-08 00:11:09 +0100

    prisma and postgres
    add a prisma client and postgres database

---
+++ components/display-posts.tsx
@@ -0,0 +1,16 @@
+import prisma from '../lib/prisma';
+
+export const getStaticProps: GetStaticProps = async () => {
+    const feed = await prisma.post.findMany({
+      where: { published: true },
+      include: {
+        author: {
+          select: { name: true },
+        },
+      },
+    });
+    return {
+      props: { feed },
+      revalidate: 10,
+    };
+  };
