a544294879e1f86769758440205d8157cbcbeb6a
Code Inverness <99823868+RobFraserCodes@users.noreply.github.com>
2023-09-11 13:00:59 +0100

    display random movies
    homepage movies are now random

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,31 +1,11 @@
-'use client'
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Post } from '@prisma/client';
 
 interface NewsPageProps {
-  posts: Pick<Post, 'id' | 'title' | 'content'>[];
+  posts: Post[];
 }
 
-export default function NewsPage({ posts: initialPosts }: NewsPageProps) {
-  const [posts, setPosts] = useState(initialPosts);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-
-    setLoading(true);
-    fetch('/api/posts')
-      .then((res) => res.json())
-      .then((data) => {
-        setPosts(data);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+const NewsPage: React.FC<NewsPageProps> = ({ posts }) => {
   return (
     <div>
       <h1>News</h1>
@@ -33,10 +13,11 @@ export default function NewsPage({ posts: initialPosts }: NewsPageProps) {
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.content}</p>
-          {/* Uncomment this if you include author information in the posts */}
-          {/* <p>Author: {post.author?.name}</p> */}
+          <p>Author: {post.author?.name}</p>
         </div>
       ))}
     </div>
   );
-}
+};
+
+export default NewsPage;
--- components/cinema.tsx
+++ components/cinema.tsx
@@ -9,14 +9,26 @@ interface nowPlaying {
     backdrop_path: string;
 }
 
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 export default function Cinema() {
     const [movies, setMovies] = useState<nowPlaying[]>([]);
+    const [randomThreeMovies, setRandomThreeMovies] = useState<nowPlaying[]>([]);
 
     useEffect(() => {
         fetch("/api/movies")
         .then((response) => response.json())
         .then((data) => {
             setMovies(data.results);
+            const shuffledMovies = shuffleArray([...data.results]);
+            setRandomThreeMovies(shuffledMovies.slice(0, 3));
         })
         .catch((error) => {
             console.error("Error fetching movies:", error);
@@ -26,40 +38,34 @@ export default function Cinema() {
   return (
     <section>
         <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+            <h2>Upcoming Movies</h2>
             {/* Movie previews */}
             <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
-            {/* Movie 1 */}
-            {Array.isArray(movies) && movies.slice(0, 3).map((movie, index) => (
-            <li key={index} className={index === 2 ? "lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1" : ""}>
+            {Array.isArray(randomThreeMovies) && randomThreeMovies.map((movie, index) => (
+                <li key={index} className={index === 2 ? "lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1" : ""}>
                 <Link href="#" className="relative block group">
-                <Image
+                    <Image
                     src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}
                     alt={movie.original_title}
                     className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
                     width={500}
                     height={500}
-                />
-
-                <div
-                className="absolute inset-0 bg-gradient-to-t from-gray-900 to-gray-900/10"
-                aria-hidden="true"/>
-            
-
-                <div
-                    className="absolute inset-0 flex flex-col items-start justify-end p-6"
-                >
-                    <h3 className="text-xl font-medium text-white">{movie.original_title}</h3>
-
-                    <span
-                    className="mt-2 inline-block bg-blue-500 rounded-full px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
-                    >
-                    Now Showing
+                    />
+                    <div
+                    className="absolute inset-0 bg-gradient-to-t from-gray-900 to-gray-900/10"
+                    aria-hidden="true"
+                    />
+                    {/* Text */}
+                    <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                    <h3 className="text-xl font-medium text-white">
+                        {movie.original_title}
+                    </h3>
+                    <span className="mt-2 inline-block bg-blue-500 rounded-full px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                        Now Showing
                     </span>
-                </div>
+                    </div>
                 </Link>
-            </li>
-
-
+                </li>
             ))}
             </ul>
         </div>
